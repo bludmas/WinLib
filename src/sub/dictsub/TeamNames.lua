19dd2d98c125308909b8b0fe89c3cdf68543a517
bludmas <152079992+bludmas@users.noreply.github.com>
2025-06-13 16:30:28 +0300

    Create TeamNames.lua

---
+++ src/sub/dictsub/TeamNames.lua
@@ -0,0 +1,20 @@
+--[[
+	INFO
+
+	You can change the names AT your own choice
+]]
+
+local TeamNames = {
+	Ally = {
+		"Eagles",
+		"Blues",
+		"Dinamians"
+	};
+	Adversary = {
+		"Wolves",
+		"Reds",
+		"Yeti"
+	};
+}
+
+return TeamNames
